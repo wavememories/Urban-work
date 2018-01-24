87d22bf7309cb7ed7d20a7f18a207fde8c6ebc46
wavememories <hirotaka.ellis@gmail.com>
2018-01-24 10:06:19 -0500

    updated app and dataprep with ending neighborhood and referencing tiny CB dataset

--- Data/dataprep.py
+++ Data/dataprep.py
@@ -3,21 +3,23 @@
 Created on Fri Jan 12 14:07:06 2018
 
 @author: hiroellis
-"""
+""" 
 
 import pandas as pd
 import json
 
-df = pd.read_csv('C:/Users/hiroellis/Desktop/urban-work/Data/citibike-trips/citibiketest.csv')
+data_path =  './Data/'
+
+df = pd.read_csv(data_path + "/citibike-trips/2013-07_tripdata.csv")
 
 print(df)
 
 from shapely.geometry import Point, shape
 
-with open('C:/Users/hiroellis/Desktop/urban-work/Data/geojson/nycnh.json') as data_file:
+with open(data_path + '/geojson/nycnh.json') as data_file:
     nh_json=json.load(data_file)
     
-def get_location_start(longitude, latitude, nh_json):
+def get_neighborhood(longitude, latitude, nh_json):
     point = Point(longitude, latitude)
     for record in nh_json['features']:
         polygon = shape(record['geometry'])
@@ -25,7 +27,9 @@ def get_location_start(longitude, latitude, nh_json):
             return record['properties']['neighborhood']
     return 'other'
 
-df['starting neighborhood'] = df.apply(lambda row: get_location_start(row['start station longitude'], row['start station latitude'],nh_json), axis=1)
+df['starting neighborhood'] = df.apply(lambda row: get_neighborhood(row['start station longitude'], row['start station latitude'],nh_json), axis=1)
+df['ending neighborhood'] = df.apply(lambda row: get_neighborhood(row['end station longitude'], row['end station latitude'],nh_json), axis=1)
+   
 
 df.to_json(orient='records')
 
